9e165c409c649bf8db3a2201a96f7231d6c05238
pavandoescode <raiyanipavan44@gmail.com>
2024-02-17 13:34:22 +0530

    middleware add

--- index.js
+++ index.js
@@ -4,58 +4,53 @@ const app = express();
 
 app.set("view engine", "ejs")
 app.use(express.static("public"))  
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
 
 
 app.get("/",(req, res)=>{
-
     res.render("index")
 })
 
 
 app.get("/contact",(req, res)=>{
-
     res.render("contact")
 })
 
 
 app.get("/AI-Image-Generator",(req, res)=>{
-
     res.render("AI-Image-Generator")
 })
 
 
 app.get("/Chat-App_-Socket-IO",(req, res)=>{
-
     res.render("Chat-App_-Socket-IO")
 })
 
 
 app.get("/URLShortenerAPI",(req, res)=>{
-
     res.render("URLShortenerAPI")
 })
 
 
 app.get("/Note-Master",(req, res)=>{
-
     res.render("Note-Master")
 })
 
 
 app.get("/Sticky-Notes",(req, res)=>{
-
     res.render("Sticky-Notes")
 })
 
 
 app.get("/Live-ISS-Tracker",(req, res)=>{
-
     res.render("Live-ISS-Tracker")
 })
 
 
 app.get("/Weather-API",(req, res)=>{
-
     res.render("Weather-API")
 })
 
