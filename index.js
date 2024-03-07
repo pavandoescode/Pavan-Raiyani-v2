00425575eb0659c913b145af691128f2ec85ad58
pavandoescode <raiyanipavan44@gmail.com>
2024-03-08 00:25:30 +0530

    err

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ routes.forEach(route => {
 // Error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
-    res.status(500).send("Something went wrong!");
+    res.status(500).send("Something went wrong!" , err.stack);
 });
 
 // Start the server
